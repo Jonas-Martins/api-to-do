ac69ea17a1f836a81d79f6abc02cdfac996691a2
Jonas Martins Linux <jonasblz@outlook.com>
2022-09-27 19:54:11 -0300

    Adiciona Migrates

--- database/migrations/2022_09_05_154002_create_tasks_table.php
+++ database/migrations/2022_09_05_154002_create_tasks_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -14,13 +15,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->nullable(false);
-            $table->string('color')->default('#FFFFFF');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->dateTime('due_date');
             $table->foreignIdFor(User::class)->references('id')->on('users')->onDelete('CASCADE');
+            $table->foreignIdFor(Category::class)->references('id')->on('categories')->onDelete('CASCADE');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -31,9 +33,10 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('categories', function(Blueprint $table){
+        Schema::table('tasks', function(Blueprint $table){
             $table->dropForeignIdFor(User::class);
+            $table->dropForeignIdFor(Category::class);
         });
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('tasks');
     }
 };
